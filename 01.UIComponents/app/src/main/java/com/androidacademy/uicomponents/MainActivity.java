58c8377da3c3fba6ec96eb30d15c60e0272e2494
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2020-01-28 12:10:14 +0600

    added corresponding java field for each ui elements

--- 01.UIComponents/app/src/main/java/com/androidacademy/uicomponents/MainActivity.java
+++ 01.UIComponents/app/src/main/java/com/androidacademy/uicomponents/MainActivity.java
@@ -3,12 +3,37 @@ package com.androidacademy.uicomponents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText customerNameEditText,customerAddressEditText,customerPhoneNoEditText,numberOfCoffeeEditText;
+    private RadioGroup coffeeTypeRadioGroup;
+    private CheckBox addExtraSugarCheckbox;
+    private Button placeOrderButton;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inflateUIElements();
+
+
+    }
+
+    private void inflateUIElements()
+    {
+        customerNameEditText = findViewById(R.id.customer_name_edit_text);
+        customerAddressEditText = findViewById(R.id.customer_address_edit_text);
+        customerPhoneNoEditText = findViewById(R.id.customer_phone_no_edit_text);
+        numberOfCoffeeEditText = findViewById(R.id.num_of_coffee_edit_text);
+        coffeeTypeRadioGroup = findViewById(R.id.coffee_type_radio_group);
+        addExtraSugarCheckbox = findViewById(R.id.extra_sugar_checkbox);
+        placeOrderButton = findViewById(R.id.place_order_button);
+
     }
 }
