8c47379c170ec95a7540dace2bdfa70047c9f18a
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2020-02-04 12:01:36 +0600

    implemented the result page backbutton and recalculate bmi button click functionalities

--- 03.SimpleBMICalculator/app/src/main/java/com/androidacademy/bmicalculator/ResultActivity.java
+++ 03.SimpleBMICalculator/app/src/main/java/com/androidacademy/bmicalculator/ResultActivity.java
@@ -1,14 +1,49 @@
 package com.androidacademy.bmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class ResultActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity
+{
+
+    private Toolbar toolbar;
+    private TextView recalculateBMIButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        inflateUIElements();
+        initUserInteractions();
+    }
+
+    private void inflateUIElements()
+    {
+        toolbar = findViewById(R.id.app_toolbar);
+        toolbar.setNavigationIcon(R.drawable.back_nav_white);
+        recalculateBMIButton = findViewById(R.id.re_calculate_bmi_button);
+        setSupportActionBar(toolbar);
+    }
+
+    private void initUserInteractions()
+    {
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        recalculateBMIButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
