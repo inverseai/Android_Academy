1c308241f7a4d300239f66b2bda4c88dd3589395
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2020-02-22 19:02:08 +0600

    implement the inflation of ui elements from xml and register onclick listener for phone no, email id and github id card view

--- MyCard/app/src/main/java/com/androidacademy/mycard/MainActivity.java
+++ MyCard/app/src/main/java/com/androidacademy/mycard/MainActivity.java
@@ -1,13 +1,53 @@
 package com.androidacademy.mycard;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.cardview.widget.CardView;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private CardView phoneNoCard, emailIdCard, githubIdCard;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inflateUIElements();
+        initUserInteractions();
+    }
+
+    private void inflateUIElements()
+    {
+        phoneNoCard = findViewById(R.id.phone_no_card);
+        emailIdCard = findViewById(R.id.email_id_card);
+        githubIdCard = findViewById(R.id.github_id_card);
+    }
+
+    private void initUserInteractions()
+    {
+        phoneNoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        emailIdCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        githubIdCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
