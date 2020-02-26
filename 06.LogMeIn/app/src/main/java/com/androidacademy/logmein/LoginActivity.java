601c398ca6e213e392fde8e4ef01550e8dbbdeeb
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2020-02-26 11:40:00 +0600

    directory name changed with app serial as prefix

--- 06.LogMeIn/app/src/main/java/com/androidacademy/logmein/LoginActivity.java
+++ 06.LogMeIn/app/src/main/java/com/androidacademy/logmein/LoginActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SignUpActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_login);
     }
 }
