09967ec28351db80766c95fd6eeef70415681fe7
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2020-02-26 12:01:14 +0600

    implemented the functionalities of inflating home screen UI element and initializing user interaction with home screen elements

--- 06.LogMeIn/app/src/main/java/com/androidacademy/logmein/MainActivity.java
+++ 06.LogMeIn/app/src/main/java/com/androidacademy/logmein/MainActivity.java
@@ -2,14 +2,60 @@ package com.androidacademy.logmein;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
+    private Button loginButton, signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inflateUIElements();
+        initUserInteractions();
+    }
+
+    private void inflateUIElements()
+    {
+        loginButton = findViewById(R.id.login_button);
+        signUpButton = findViewById(R.id.sign_up_button);
+    }
+
+
+    private void initUserInteractions()
+    {
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                moveToLoginScreen();
+
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                moveToSignUpScreen();
+            }
+        });
+    }
+
+    private void moveToLoginScreen()
+    {
+
+
+    }
+
+    private void moveToSignUpScreen()
+    {
+
     }
 }
